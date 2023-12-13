6884e12ed191f224a9a5cde1dea803fee34b4ef2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 13:54:22 +0900

    Update vanishing.swift

--- vanishing.swift
+++ vanishing.swift
@@ -1,14 +1,9 @@
 import Foundation
 
-let filePath = ProcessInfo.processInfo.environment["PWD"]! + (ProcessInfo.processInfo.operatingSystem == .macOS ? "/input.txt" : "/dev/stdin")
-let fileURL = URL(fileURLWithPath: filePath)
-let input = try! String(contentsOf: fileURL).trimmingCharacters(in: .newlines)
+let filePath = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : "/dev/stdin"
+let input = try! String(contentsOfFile: filePath).trimmingCharacters(in: .newlines)
 
-let values = input.split(separator: " ").map { Int($0)! }
+let values = input.split(separator: " ").compactMap { Int($0) }
 let (V, T, S, D) = (values[0], values[1], values[2], values[3])
 
-if V * T <= D && D <= V * S {
-    print("No")
-} else {
-    print("Yes")
-}
+print(V * T <= D && D <= V * S ? "No" : "Yes")
